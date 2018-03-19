d369a40c8ee6c3c48c4f2ab3f1f70b8cee75652a
taro <industrial_communication@yahoo.co.jp>
2018-03-19 23:54:38 +0900

    push

--- app/src/main/java/esetomin/com/samplenc/common/Const.java
+++ app/src/main/java/esetomin/com/samplenc/common/Const.java
@@ -2,19 +2,12 @@ package esetomin.com.samplenc.common;
 
 public class Const {
 
-    /** 画面ID_入力画面 */
-    public static final String SCREEN_ID_EDIT = "SCREEN_ID_EDIT";
-    /** 画面ID_ランキング画面 */
-    public static final String SCREEN_ID_RANKING = "SCREEN_ID_RANKING";
-    /** 画面ID_作者PR画面 */
-    public static final String SCREEN_ID_PROFILE = "SCREEN_ID_PROFILE";
-
     /** NCMBパラメータ_アプリ名 */
-    public static final String APP_NAME = "samplenc";
+    public static final String APP_NAME = "xxxxxxx";
     /** NCMBパラメータ_アプリケーションキー */
-    public static final String APP_KEY = "6485be163f52001e1a8719fec6215cda9de3d15f08759fa54c516b0bbcc503b9";
+    public static final String APP_KEY = "xxxxxxx";
     /** NCMBパラメータ_クライアントキー */
-    public static final String CLIENT_KEY = "da6a29429a165e4db917377441964a15eb052872955aa45080e2bb6de16a9a24";
+    public static final String CLIENT_KEY = "xxxxxxx";
 
     /** NCMBパラメータ_順位 */
     public static final String NCMB_PARAM_RANK = "NCMB_PARAM_RANK";
